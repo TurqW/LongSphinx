93b1914c7fe88e9da190eb6c07d62c073c69ea73
walterw9000 <walterw9000@users.noreply.github.com>
2018-12-17 21:47:05 -0800

    Major generator overhaul to allow for future actual *use* of generated things

--- generator.py
+++ generator.py
@@ -1,47 +1,57 @@
-import random
-import logging
-import yaml
 from num2words import num2words
-import re
 import configmanager
+import copy
+import logging
 import mcgenerator
+import random
+import re
+import yaml
 log = logging.getLogger('LongSphinx.Generators')
 
 conf = {}
 
 def generate(name):
-	if name.startswith('mc:'):
+	if name.startswith('mc.'):
 		#TODO: There may be a less hardcoded way to do this.
-		return mcgenerator.generate(name[3:])
-	if name.startswith('num:'):
-		rangeends = name.strip("num: ").split('-')
-		return num2words(random.randrange(int(rangeends[0]), int(rangeends[1])))
-	parsed_name = name.split('.')
-	generator = parsed_name[0]
-	load_config(generator)
-	if len(parsed_name) == 1:
-		result = random.choice(conf[generator].config['root'])
+		result = {'text': mcgenerator.generate(name[3:])}
+	elif name.startswith('num.'):
+		rangeends = name.strip("num. ").split('-')
+		result = {'text': num2words(random.randrange(int(rangeends[0]), int(rangeends[1])))}
 	else:
-		option_set = conf[generator].config
-		for level in parsed_name[1:]:
-			option_set = option_set[level]
-		result = random.choice(option_set)
-	return populate_string(result)
-	
+		parsed_name = name.split('.')
+		generator = parsed_name[0]
+		load_config(generator)
+		if len(parsed_name) == 1:
+			result = copy.deepcopy(random.choice(conf[generator].config['root']))
+		else:
+			option_set = conf[generator].config
+			for level in parsed_name[1:]:
+				option_set = option_set[level]
+			result = copy.deepcopy(random.choice(option_set))
+	print(result)
+	return populate(result)
 
-def populate_string(partial):
-	for toReplace in re.findall(r'{[\w.:-]+}', partial):
-		partial = partial.replace(toReplace, generate(toReplace[1:-1]), 1)
-	return fix_articles(partial)
-
-def fix_articles(text):
-	# relevant: https://stackoverflow.com/questions/2763750/how-to-replace-only-part-of-the-match-with-python-re-sub
-	return re.sub(r"(^|\W)a( [aAeEiIoOuU](?!ni))", r'\1an\2', text)
 
 def load_config(name):
 	if name not in conf:
 		conf[name] = configmanager.ConfigManager('genConfig/{0}.yaml'.format(name))
 	conf[name].update_config()
+
+def populate(object):
+	for key in (key for key in object.keys() if key != 'text'):
+		object[key] = generate(object[key])
+	return object
+
+def extract_text(object):
+	text = object['text']
+	fillings = {}
+	for key in (key for key in object.keys() if key != 'text'):
+		fillings[key] = extract_text(object[key])
+	return fix_articles(text.format(**fillings))
+
+def fix_articles(text):
+	# relevant: https://stackoverflow.com/questions/2763750/how-to-replace-only-part-of-the-match-with-python-re-sub
+	return re.sub(r"(^|\W)a( [aAeEiIoOuU](?!ni))", r'\1an\2', text)
 
 def readme(generators):
 	msg = ''
