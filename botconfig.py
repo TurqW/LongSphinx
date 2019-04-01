b40ed1746425a19d34e822ee1288ba064760885c
walterw9000 <walterw9000@users.noreply.github.com>
2019-04-01 13:27:26 -0700

    Fix breakage caused by previous change

--- botconfig.py
+++ botconfig.py
@@ -8,13 +8,13 @@ conf = configmanager.ConfigManager(filename)
 def get_object(server, *hierarchy):
 	try:
 		found = conf.config['servers'][server.id]
-	except KeyError:
+	except (KeyError, AttributeError):
 		found = conf.config['servers']['default']
 	for level in hierarchy:
 		try:
 			found = found[level]
 		except:
-			log.error('Could not find {0} out of {1} on server {2}'.format(level, hierarchy, server_id))
+			log.error('Could not find {0} out of {1} on server {2}'.format(level, hierarchy, server.id if hasattr(server, 'id') else 'default'))
 			return {}
 	return found
 
