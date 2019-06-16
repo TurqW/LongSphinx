416e436c5a7f977eff362d15d66a29b95b829956
walterw9000 <walterw9000@users.noreply.github.com>
2019-06-16 15:38:48 -0700

    Minor generator tweaks, committing generator test script

--- gentest.py
+++ gentest.py
@@ -1,4 +1,15 @@
 import generator
+from os import listdir
+from os.path import isfile, join
+import sys
 
-for _ in range(100):
-  print(generator.extract_text(generator.generate('insult')))
+if len(sys.argv) > 1:
+	for _ in range(1000):
+		print(generator.extract_text(generator.generate(sys.argv[1])))
+else:
+	files = [f for f in listdir('genConfig') if isfile(join('genConfig', f))]
+
+	for file in files:
+		print(file)
+		for _ in range(1000):
+			print(generator.extract_text(generator.generate(file.split('.')[0])))
